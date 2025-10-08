a0fd3dcf74c071a4a17582db931e260bd917f6ba
Adam J.A <22190@ecam.be>
2025-10-08 10:45:02 +0200

    modifreverseweel

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,56 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
- 
+
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *motor1 = AFMS.getMotor(1);
 const int encoderPin = 2;
 volatile int pulseCount = 0;
 void countPulse();
- 
- 
-void setup() 
-{
- 
+
+void setup() {
   pinMode(encoderPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(encoderPin), countPulse, RISING);
-  Serial.begin(9600);                
+  Serial.begin(9600);
   Serial.println("Demarrage du programme...");
- 
+
   if (!AFMS.begin()) {
     Serial.println("Erreur : Motor Shield non detectee !");
-    while (1);                      
+    while (1);
   }
   Serial.println("Motor Shield OK !");
- 
+
   motor1->setSpeed(150);
   motor1->run(FORWARD);
   Serial.println("Moteur 1 tourne vers l'avant a 150");
 }
-void loop() 
-{
+
+void loop() {
+ 
   motor1->setSpeed(200);
   motor1->run(FORWARD);
   Serial.println("Moteur 1 avance a 200");
   Serial.println(pulseCount);
   delay(3000);
- 
+
   motor1->run(RELEASE);
   Serial.println("Moteur 1 arrete");
   Serial.println(pulseCount);
   delay(1000);
+
  
-  motor1->setSpeed(200);
+  motor1->setSpeed(200);  
   motor1->run(BACKWARD);
   Serial.println("Moteur 1 recule a 200");
   Serial.println(pulseCount);
   delay(3000);
- 
-//   commssss
+
+  
   motor1->run(RELEASE);
   Serial.println("Moteur 1 arrete");
   Serial.println(pulseCount);
   delay(1000);
 }
- 
+
 void countPulse() {
   pulseCount++;
-  
 }
-// End of file: src/main.cpp
